6b8accffa802c1098e628a8929e3dd26ca25986a
mephi82 <48950026+mephi82@users.noreply.github.com>
2021-05-17 23:06:12 +0900

    Update Water.py

--- Water.py
+++ Water.py
@@ -3,43 +3,55 @@ import json
 from statistics import mean
 import mariadb
 
-if len(sys.argv) == 4:
-    RACK, FLOOR, PIPE = sys.argv[1:4]
+if len(sys.argv) == 5:
+    RACK, FLOOR, PIPE, PORT = sys.argv[1:5]
 else:
     print('3 arguments required. Now:', sys.argv)
     sys.exit(1)
 
-SAMPLING = 5
+SAMPLING = 60
 
 def emptyRecord():
     records = {'temp':[], 'ec':[], 'ph':[]}
     return(records)
 
-def DBwrite_water(cursor, temperature, ph, ec):
-    if ph is not None:
-        cursor.execute("INSERT INTO `water.tab` (rack,floor,pipe,temperature,ph,ec) VALUES (?,?,?,?,?,?)",
-                  (RACK,FLOOR,PIPE,temperature,ph,ec))
-    else:
-        cursor.execute("INSERT INTO `water.tab` (rack,floor,pipe,temperature,ec) VALUES (?,?,?,?,?)",
-                  (RACK,FLOOR,PIPE,temperature,ec))
+def DBwrite_water(conn, temperature, ph, ec):
+    cursor = conn.cursor()
+    try:
+        if ph is not None:
+            cursor.execute("INSERT INTO `water.tab` (rack,floor,pipe,temperature,ph,ec) VALUES (?,?,?,?,?,?)",
+                      (RACK,FLOOR,PIPE,temperature,ph,ec))
+        else:
+            cursor.execute("INSERT INTO `water.tab` (rack,floor,pipe,temperature,ec) VALUES (?,?,?,?,?)",
+                      (RACK,FLOOR,PIPE,temperature,ec))
+        conn.commit()
+    except:
+        conn = getConnDB()
+        
+    return(conn)
 
-try:
-    conn = mariadb.connect(
-        user="root",
-        password="!Gkrrhkwkd0690",
-        host="220.149.87.248",
-        port=3307,
-        database="livfarm"
+def getConnDB():
+    conn= None
+    try:
+        conn = mariadb.connect(
+            user="root",
+            password="!Gkrrhkwkd0690",
+            host="220.149.87.248",
+            port=3307,
+            database="livfarm"
 
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
+        )
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+    return(conn);
+
 
 # Get Cursor
-cur = conn.cursor()
+conn = getConnDB()
 
-T = serial.Serial('COM6',115200)
+
+T = serial.Serial(PORT,115200)
 for _ in range(10):
     T.readline()
 records = emptyRecord()
@@ -48,12 +60,12 @@ while True:
     if count>SAMPLING:
         print("Writing DB")
         if len(records['ph'])>0:
-            DBwrite_water(cur,mean(records['temp']), mean(records['ph']),mean(records['ec']))
+            conn = DBwrite_water(conn,mean(records['temp']), mean(records['ph']),mean(records['ec']))
         else:
-            DBwrite_water(cur,mean(records['temp']), None,mean(records['ec']))
+            conn = DBwrite_water(conn,mean(records['temp']), None,mean(records['ec']))
         count = 0        
         records = emptyRecord()
-        conn.commit()
+        
 
     output = T.readline().decode('ascii')
     if output.startswith('{'):
