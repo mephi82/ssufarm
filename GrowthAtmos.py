d2771bddca9c2676be14c9e611dd7b2a12ad47ec
mephi82 <48950026+mephi82@users.noreply.github.com>
2021-05-24 10:40:16 +0900

    Update GrowthAtmos.py

--- GrowthAtmos.py
+++ GrowthAtmos.py
@@ -133,9 +133,10 @@ while True:
         print(pixels, temp, hum)
 
     if capcount%3600==0:
-        imgpath = 'images/'
+        imgpath = '/images/'
         imgname = 'img_'+RACK+FLOOR+PIPE+POT+str(int(time.time()*1000.0))+'.jpg'
-        cv2.imwrite(imgname, img)
+        cv2.imwrite(imgpath+imgname, img)
+        print(imgpath+imgname)
         imgurl = '/web/livfarm/'+imgname
         try:
             transport.connect(username = 'hod', password = 'gkrrhkwkd0690')
