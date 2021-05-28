f318ba4086e277834f6f47e1394e1f629b8c2297
mephi82 <48950026+mephi82@users.noreply.github.com>
2021-05-28 22:02:48 +0900

    Update GrowthAtmos.py

--- GrowthAtmos.py
+++ GrowthAtmos.py
@@ -49,8 +49,9 @@ ONETIME_H_RES = 0x20
 camera = PiCamera()
 camera.resolution = (672, 512)
 camera.framerate = 32
-camera.brightness = 55
+camera.brightness = 60
 camera.contrast = 10
+camera.sharpness = 50
 camera.image_effect = 'colorpoint'
 
 # camera.exposure_mode = 'nightpreview'
